18c40b36ed5c817281e43cc4555182a20a901011
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-10-17 00:54:07 +0200

    Add token generator

--- internal/server/jwt.go
+++ internal/server/jwt.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"factuurhub/internal/conf"
+	"factuurhub/internal/store"
+	"fmt"
+	"time"
 
 	"github.com/cristalhq/jwt/v3"
 	"github.com/rs/zerolog/log"
@@ -25,4 +28,17 @@ func jwtSetup(conf conf.Config) {
 	if err != nil {
 		log.Panic().Err(err).Msg("Error creating JWT verifier")
 	}
+}
+
+func generateJWT(user *store.User) string {
+	claims := &jwt.RegisteredClaims{
+		ID:        fmt.Sprint(user.ID),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 7)),
+	}
+	builder := jwt.NewBuilder(jwtSigner)
+	token, err := builder.Build(claims)
+	if err != nil {
+		log.Panic().Err(err).Msg("Error building JWT")
+	}
+	return token.String()
 }
