c51e80f25bb58d93d6925c52e09c54a4d35d8189
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-10-16 21:41:14 +0200

    Add DB connection to server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,13 @@
 package server
 
+import (
+	"factuurhub/internal/database"
+	"factuurhub/internal/store"
+)
+
 func Start() {
+	store.SetDBConnection(database.NewDBOptions())
+
 	router := setRouter()
 	router.Run(":8080")
 }
