8538c8f71261e4dc40dbe4f613a7fc872bdcd9af
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-10-16 02:54:20 +0200

    Create Gin server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		api.GET("/hello", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{"msg": "world"})
+		})
+	}
+
+	router.NoRoute(func(ctx *gin.Context) { ctx.JSON(http.StatusNotFound, gin.H{}) })
+	router.Run(":8080")
+}
