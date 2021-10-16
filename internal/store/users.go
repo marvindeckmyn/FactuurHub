d7ecfde28c115fa40c2422fabaad43c7374d9b24
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-10-16 21:41:46 +0200

    Implement AddUser & Authenticate

--- internal/store/users.go
+++ internal/store/users.go
@@ -1,8 +1,29 @@
 package store
 
+import "errors"
+
 type User struct {
+	ID       int
 	Username string `binding:"required,min=3,max=30"`
 	Password string `binding:"required,min=8"`
 }
 
-var Users []*User
+func AddUser(user *User) error {
+	_, err := db.Model(user).Returning("*").Insert()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Authenticate(username, password string) (*User, error) {
+	user := new(User)
+	if err := db.Model(user).Where(
+		"username = ?", username).Select(); err != nil {
+		return nil, err
+	}
+	if password != user.Password {
+		return nil, errors.New("Wachtwoord ongeldig.")
+	}
+	return user, nil
+}
