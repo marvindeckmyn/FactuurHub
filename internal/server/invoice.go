3f3edd54c9d95975f0dc3967cb6bf56811b3fcd8
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-10-17 02:07:01 +0200

    Add invoice file for server to be able to create an invoice

---
+++ internal/server/invoice.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"errors"
+	"factuurhub/internal/store"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
+
+func createInvoice(ctx *gin.Context) {
+	invoice := new(store.Invoice)
+	if err := ctx.Bind(invoice); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err := currentUser(ctx)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	if err := store.AddInvoice(user, invoice); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg":  "Invoice created successfully",
+		"data": invoice,
+	})
+}
+
+func currentUser(ctx *gin.Context) (*store.User, error) {
+	var err error
+	_user, exists := ctx.Get("user")
+	if !exists {
+		err = errors.New("Current context user not set")
+		log.Error().Err(err).Msg("")
+		return nil, err
+	}
+	user, ok := _user.(*store.User)
+	if !ok {
+		err = errors.New("Context user is not valid type")
+		log.Error().Err(err).Msg("")
+		return nil, err
+	}
+	return user, nil
+}
