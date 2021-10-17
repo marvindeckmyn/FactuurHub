cc00f0590169d92bf878079af91d7bfbd184215b
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-10-17 14:26:19 +0200

    Add registration

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header/Header';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header />
+      <div className="container d-flex align-items-center flex-column">
+        <Switch>
+          <Route path="/" exact={true}>
+            <RegistrationForm />
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
---
+++ assets/src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,100 @@
+import React, {useState} from "react";
+import axios from 'axios';
+import {API_BASE_URL} from '../../constants/apiConstants';
+import { withRouter } from "react-router-dom";
+function RegistrationForm(props) {
+
+    const [state, setState] = useState ({
+        username : "",
+        password : "",
+        confirmPassword: "",
+        successMessage: null
+    })
+
+    const handleChange = (e) => {
+        const {id, value} = e.target
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+        if(state.password === state.confirmPassword) {
+            sendDetailsToServer()
+        } else {
+            props.showError("Wachtwoorden komen niet overeen");
+        }
+    }
+
+    const sendDetailsToServer = () => {
+        if(state.username.length && state.password.length) {
+            props.showError(null);
+            const payload={
+                "username":state.username,
+                "password":state.password,
+            }
+            axios.post(API_BASE_URL+'/signup', payload)
+                .then(function (response) {
+                    if(response.status === 200) {
+                        setState(prevState => ({
+                            ...prevState,
+                            'successMessage' : 'Registratie succesvol. Je wordt gestuurd naar de startpagina...'
+                        }))
+                        redirectToHome();
+                        props.showError(null)
+                    } else {
+                        props.showError("Error");
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error)
+                });
+        } else {
+            props.showError('Vul een geldige username en wachtwoord in')
+        }
+    }
+
+    const redirectToHome = () => {
+        props.history.push('/home');
+    }
+
+    const redirectToLogin = () => {
+        props.updateTitle('Factuurhub - Login');
+        props.history.push('/login');
+    }
+    return(
+        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+            <h1>Registratie</h1>
+            <form>
+                <div className="form-group text-left">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" className="form-control" id="username" placeholder="Vul je username in" value={state.username} onChange={handleChange} required/>
+                </div>
+
+                <div className="form-group text-left">
+                    <label htmlFor="password">Wachtwoord</label>
+                    <input type="password" className="form-control" id="password" placeholder="Vul je wachtwoord in" value={state.password} onChange={handleChange} required/>
+                </div>
+
+                <div className="form-group text-left">
+                    <label htmlFor="password">Bevestig wachtwoord</label>
+                    <input type="password" className="form-control" id="confirmPassword" placeholder="Bevestig je wachtwoord" value={state.confirmPassword} onChange={handleChange} required/>
+                </div>
+                
+                <button type="submit" className="btn btn-primary" onClick={handleSubmitClick}>Registreer</button>
+            </form>
+            <div className="alert alert-success mt-2" style={{display: state.successMessage ? 'block' : 'none' }} role="alert">
+                {state.successMessage}
+            </div>
+
+            <div classname="mt-2">
+                <span>Heb je al een account?</span>
+                <span className="loginText" onClick={() => redirectToLogin()}>Login</span>
+            </div>
+        </div>
+    )
+}
+
+export default withRouter(RegistrationForm);
